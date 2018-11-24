e671261b7a6c0d220dc2e8c82f5288cc17d2f81e
Black Hawk <taghikhani.sh@gmail.com>
2018-11-23 20:25:19 -0500

    Iterator implementation

---
+++ src/Board.java
@@ -0,0 +1,144 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.MinPQ;
+
+public class Board {
+
+    private Integer dimention ;
+    private Integer haming = 0;
+    private Integer manhattan = 0;
+    private  Integer twin;
+    private int[][] Board;
+    private int blanki;
+    private int blankj;
+
+    public Board(int[][] blocks)  {
+
+        if (blocks == null){throw new IllegalArgumentException();}
+
+        dimention = blocks.length;
+        Board = new int[blocks.length][blocks.length];
+
+        for(int i = 0; i < blocks.length; i++){
+            for (int j = 0; j<blocks.length; j++ ){
+
+                Board[i][j] = blocks[i][j];
+
+                if(Board[i][j] != i*blocks.length + j+1){
+                    haming++;
+                }
+
+                if(Board[i][j] != i*blocks.length + j+1){
+                    int distance;
+                    int quotint = Board[i][j] / blocks.length;
+                    int remainder = Board[i][j] % blocks.length;
+
+                    distance = quotint + remainder;
+
+                    manhattan = manhattan + distance;
+
+                }
+
+                if (Board[i][j] == 0){
+                    blanki = i;
+                    blankj = j;
+                }
+
+            }
+        }
+
+    }         // construct a board from an n-by-n array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+
+    public int dimension(){return dimention;}        // board dimension n
+    public int hamming(){return  haming;}       // number of blocks out of place
+    public int manhattan(){return manhattan;}                // sum of Manhattan distances between blocks and goal
+    public boolean isGoal(){return (manhattan == 0 && haming == 0);}     // is this board the goal board?
+    public Board twin(){
+
+        int[][] twinb = new int[dimention][dimention];
+        Board twinBoard;
+        int Rand = StdRandom.uniform(2);
+        int randi = StdRandom.uniform(dimention);
+        int randj = StdRandom.uniform(dimention);
+
+        twin = twinb[randi][randj];
+
+        if(Rand == 0) {
+            if (randi != dimention - 1) {
+                twinb[randi][randj] = twinb[randi + 1][randj];
+                twinb[randi + 1][randj] = twin;
+
+            } else {
+                twinb[randi][randj] = twinb[randi - 1][randj];
+                twinb[randi - 1][randj] = twin;
+            }
+        } else{
+            if (randj != dimention - 1) {
+                twinb[randi][randj] = twinb[randi][randj+1];
+                twinb[randi][randj+1] = twin;
+
+            } else {
+                twinb[randi][randj] = twinb[randi][randj-1];
+                twinb[randi][randj-1] = twin;
+            }
+
+        }
+
+        twinBoard = new Board(twinb);
+        return twinBoard;
+
+
+    } // a board that is obtained by exchanging any pair of blocks
+    public boolean equals(Object y){return y.equals(Board);}   // does this board equal y?
+
+    public Iterable<Board> neighbors(){
+        Board nneighbor;
+        Board sneighbor;
+        Board wneighbor;
+        Board eneighbor;
+        MinPQ<Board> neighbors = new MinPQ<>();
+
+        if(blanki!=0){
+            nneighbor = new Board(this.swap(blanki-1, blankj));
+            neighbors.insert(nneighbor);
+        }
+        if(blankj!=0){
+            wneighbor = new Board(this.swap(blanki, blankj-1));
+            neighbors.insert(wneighbor);
+        }
+        if(blanki != dimention-1){
+            sneighbor = new Board(this.swap(blanki+1, blankj));
+            neighbors.insert(sneighbor);
+        }
+        if(blankj != dimention-1){
+            eneighbor= new Board(this.swap(blanki, blankj+1));
+            neighbors.insert(eneighbor);
+        }
+
+
+        public boolean hasNext() {
+            neighbors.iterator().hasNext();
+        }
+
+        public Board next(){
+
+            neighbors.iterator().next();
+
+        }
+
+
+
+
+    }  // all neighboring boards
+    public String toString()              {} // string representation of this board (in the output format specified below)
+
+    public static void main(String[] args) {}// unit tests (not graded)
+
+    private int[][] swap(int i, int j){
+        Board[blankj][blankj] = Board[i][j];
+        Board[i][j] = 0;
+        return Board;
+    }
+
+
+}
